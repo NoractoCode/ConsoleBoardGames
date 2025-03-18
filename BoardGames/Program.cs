852d6912521c9b497c989b8e40a9abb2ea8f258a
NoractoCode <139512596+NoractoCode@users.noreply.github.com>
2025-03-18 17:52:57 +1000

    Add files via upload

--- BoardGames/Program.cs
+++ BoardGames/Program.cs
@@ -5,8 +5,12 @@
 
         static void Main()
         {
-            Console.Clear();
+            
             int x = 0;
+<<<<<<< HEAD
+
+            do
+=======
             Console.WriteLine("HEY! Welcomer ot my collection of Board Games!");
             Console.WriteLine("Which one would you like to play?");
             Console.WriteLine("\n");
@@ -35,11 +39,38 @@
                 game.GameStart();
             }
             else
+>>>>>>> 0b3a5af0d3da002ba488c7353265dee909368de6
             {
+                Console.Clear();
+                Console.WriteLine("HEY! Welcome to my collection of Board Games!");
+                Console.WriteLine("Which one would you like to play?");
+                Console.WriteLine("\n");
+                Console.WriteLine("(1) Tic Tac Toe");
+                Console.WriteLine("(2) Connect Four");
+                string choice = Console.ReadLine();
+                try
+                {
+                    x = Convert.ToInt32(choice);
+                }
+                catch { }
+                if (x == 1)
+                {
+                    TicTacToe game = new TicTacToe(3, 3);
+                    game.GameStart();
+                }
+                else if (x == 2)
+                {
+                    ConnectFour game = new ConnectFour(7, 6);
+                    game.GameStart();
+                }
+                else
+                {
+
+                }
                 Console.WriteLine("Invalid Input");
                 Thread.Sleep(1000);
-                Main();
             }
+           while(x == 0 || x>2);
 
         }
     }
