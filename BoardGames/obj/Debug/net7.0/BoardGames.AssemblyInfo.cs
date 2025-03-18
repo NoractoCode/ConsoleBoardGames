f781c7bf86665f526ed9da346e7baadd9e9af2de
NoractoCode <139512596+NoractoCode@users.noreply.github.com>
2025-03-18 18:03:38 +1000

    Updated Menu Code

--- BoardGames/obj/Debug/net7.0/BoardGames.AssemblyInfo.cs
+++ BoardGames/obj/Debug/net7.0/BoardGames.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BoardGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b3a5af0d3da002ba488c7353265dee909368de6")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4256d49b324ce67234234da1cb8ee46d8dbdedb9")]
->>>>>>> 0b3a5af0d3da002ba488c7353265dee909368de6
 [assembly: System.Reflection.AssemblyProductAttribute("BoardGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoardGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
